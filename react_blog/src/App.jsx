fcc9b27e00032ae95ad066f8e2612a4f895b4682
Gustavo Bizarro Lombardo <90002024+Dev1Bizarro@users.noreply.github.com>
2023-10-23 14:11:26 -0300

    critando pages e rotas

--- react_blog/src/App.jsx
+++ react_blog/src/App.jsx
@@ -7,6 +7,8 @@ import Home from "./pages/Home/Home"
 import About from "./pages/About/About"
 import Navbar from "./components/Navbar"
 import Footer from "./components/Footer"
+import Register from "./pages/Register/Register"
+import Login from "./pages/Login/Login"
 
 
 
@@ -19,6 +21,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />  
       </Routes>
 </div>
 <Footer />
--- react_blog/src/firebase/config.jsx
+++ react_blog/src/firebase/config.jsx
@@ -1,8 +1,8 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firebase";
 
-// Your web app's Firebase configuration
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+
 const firebaseConfig = {
   apiKey: "AIzaSyCNp-ZUIMmJ90o1UYGnGVFBZzpJ3eXEIm4",
   authDomain: "reactblog-a56bb.firebaseapp.com",
